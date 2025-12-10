774c1b7b086059c7ccdbffec285e6add1edc167f
jross-mm <jonathan.ross@made.media>
2025-12-10 23:00:06 +0000

    Add ORM repository factory and API service

--- src/repositories/orm/__init__.py
+++ src/repositories/orm/__init__.py
@@ -2,6 +2,7 @@
 
 from src.repositories.orm.course_repository import CourseRepository
 from src.repositories.orm.department_repository import DepartmentRepository
+from src.repositories.orm.factory import ORMRepositoryFactory
 from src.repositories.orm.lecturer_repository import LecturerRepository
 from src.repositories.orm.programme_repository import ProgrammeRepository
 from src.repositories.orm.research_repository import ResearchProjectRepository
@@ -12,6 +13,7 @@ __all__ = [
     "CourseRepository",
     "DepartmentRepository",
     "LecturerRepository",
+    "ORMRepositoryFactory",
     "ProgrammeRepository",
     "ResearchProjectRepository",
     "StaffRepository",
--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,5 +1,6 @@
 """Service layer for data access orchestration."""
 
 from src.services.api_service import APIService
+from src.services.orm_api_service import ORMAPIService
 
-__all__ = ["APIService"]
+__all__ = ["APIService", "ORMAPIService"]
