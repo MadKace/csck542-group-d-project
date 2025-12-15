112c28ce2625fb3aa98d0e002a92af27ee330263
I.A.A.Ghafoor <i.a.a.ghafoor@liverpool.ac.uk>
2025-12-15 01:33:55 +0000

    feat: Data from Repositories
    Data from various repositories

--- gui/gui-rewrite.py
+++ gui/gui-rewrite.py
@@ -11,3 +11,25 @@ def init_database():
 
 if  __name__ == '__main__':
     init_database()
+
+api = APIService()
+
+# Load all data from repositories
+
+students = api.student_repo.get_all()
+all_students_df = pd.DataFrame([student.as_dict for student in students])
+
+lecturers = api.lecturer_repo.get_all()
+all_lecturers_df = pd.DataFrame([lecturer.as_dict for lecturer in lecturers])
+
+staff = api.staff_repo.get_all()
+all_staff_df = pd.DataFrame([staff.as_dict for staff in staff])
+
+courses = api.course_repo.get_all()
+all_courses_df = pd.DataFrame([course.as_dict for course in courses])
+
+departments = api.department_repo.get_all()
+all_departments_df = pd.DataFrame([department.as_dict for department in departments])
+
+research_projects = api.research_project_repo.get_all()
+all_projects_df = pd.DataFrame([rp.as_dict for rp in research_projects])
